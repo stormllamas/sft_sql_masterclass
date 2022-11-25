72a8cb86814ab56ae4d4753ed5b431f4b32d97cf
Joaquim Ignatius Llamas <joaquimignatius.llamas@softvision.com>
2022-11-25 12:56:09 +0800

    Assignment 1

---
+++ SQLQuery1.sql
@@ -0,0 +1,25 @@
+SELECT
+CustomerId,
+Count(CASE
+		WHEN (OrderDate BETWEEN '20170101' and '20181212') AND ShippedDate is null
+		THEN 1 ELSE null END
+	) as OrderCount
+FROM dbo.[Order]
+GROUP BY CustomerId
+HAVING Count(CASE
+		WHEN (OrderDate BETWEEN '20170101' and '20181212') AND ShippedDate is null
+		THEN 1 ELSE null END
+	)>1
+
+	
+SELECT *
+INTO dbo.ProductBackup
+FROM dbo.Product
+WHERE ModelYear < 2016 OR ModelYear > 2016;
+
+
+UPDATE dbo.ProductBackup
+SET ListPrice = ListPrice*1.2
+WHERE
+	BrandId IN (SELECT BrandId FROM dbo.Brand WHERE BrandName='Heller')
+	OR BrandId IN (SELECT BrandId FROM dbo.Brand WHERE BrandName='Sun Bicycles')
